f3af2400390c88e9393423c2b26f281386725422
Onat Bas <git@onat.me>
2025-04-26 13:52:23 -0400

    Initial Commit

---
+++ otracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  otracker
+//
+//  Created by Onat Bas on 2025-04-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ otrackerTests/otrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  otrackerTests.swift
+//  otrackerTests
+//
+//  Created by Onat Bas on 2025-04-26.
+//
+
+import Testing
+@testable import otracker
+
+struct otrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
